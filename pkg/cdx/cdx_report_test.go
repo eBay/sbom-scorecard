87f7064ec20d0849a24b29126162dae1b4f0de91
Justin Abrahms <jabrahms@ebay.com>
2023-01-12 09:21:29 -0800

    Refactor assertions in tests

--- pkg/cdx/cdx_report_test.go
+++ pkg/cdx/cdx_report_test.go
@@ -7,54 +7,53 @@ import (
 	"opensource.ebay.com/sbom-scorecard/pkg/scorecard"
 )
 
+func assertTextEqual(t *testing.T, actual string, expected string) {
+	if strings.Trim(actual, " \n") != expected {
+		t.Log("Incorrect report text generated.\n" +
+			"Got this:\n" + actual + "\n\n but expected:\n" + expected)
+		t.Fail()
+	}
+}
+
 func TestCycloneE2eReport(t *testing.T) {
 	r := GetCycloneDXReport("../../examples/dropwizard.cyclonedx.json")
 
 	report_text := r.Report()
-
-	if strings.Trim(report_text, " \n") != `167 total packages
+	assertTextEqual(t,
+		report_text,
+		`167 total packages
 79% have licenses.
 100% have package digest.
 100% have purls.
 0% have CPEs.
 Has creation info? true
-Spec valid? true` {
-		t.Log("Incorrect report text generated.\n" +
-			"Got this:\n" + report_text)
-		t.Fail()
-	}
+Spec valid? true`)
 }
 
 func TestCycloneE2eGrade(t *testing.T) {
 	r := GetCycloneDXReport("../../examples/dropwizard.cyclonedx.json")
 
 	report_text := scorecard.Grade(r)
-
-	if strings.Trim(report_text, " \n") != `Spec Compliance: 25/25
+	assertTextEqual(t,
+		report_text,
+		`Spec Compliance: 25/25
 Package ID: 10/20 (100% have purls and 0% have CPEs)
 Package Versions: 20/20
 Package Licenses: 15/20
 Creation Info: 15/15
-Total points: 85/100 or 85%` {
-		t.Log("Incorrect report text generated.\n" +
-			"Got this:\n" + report_text)
-		t.Fail()
-	}
+Total points: 85/100 or 85%`)
 }
 
 func TestCycloneXML(t *testing.T) {
 	r := GetCycloneDXReport("../../examples/openfeature-javasdk.cyclonedx.xml")
 
 	report_text := scorecard.Grade(r)
-
-	if strings.Trim(report_text, " \n") != `Spec Compliance: 25/25
+	assertTextEqual(t,
+		report_text,
+		`Spec Compliance: 25/25
 Package ID: 10/20 (100% have purls and 0% have CPEs)
 Package Versions: 20/20
 Package Licenses: 18/20
 Creation Info: 15/15
-Total points: 88/100 or 88%` {
-		t.Log("Incorrect report text generated.\n" +
-			"Got this:\n" + report_text)
-		t.Fail()
-	}
+Total points: 88/100 or 88%`)
 }
