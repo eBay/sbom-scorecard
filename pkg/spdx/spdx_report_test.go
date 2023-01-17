953de4ef361a43cd3cd05529cc6164da361c487f
John Speed Meyers <jsmeyers@chainguard.dev>
2023-01-17 13:34:14 -0500

    Rewrite tests as table tests
    And add test for photon document to ensure correct calculation
    of package version and license information.
    
    Signed-off-by: John Speed Meyers <jsmeyers@chainguard.dev>

--- pkg/spdx/spdx_report_test.go
+++ pkg/spdx/spdx_report_test.go
@@ -7,11 +7,11 @@ import (
 	"github.com/ebay/sbom-scorecard/pkg/scorecard"
 )
 
-func TestSpdxE2eReport(t *testing.T) {
-	r := GetSpdxReport("../../examples/julia.spdx.json")
-	report_text := r.Report()
-
-	if strings.Trim(report_text, " \n") != `34 total packages
+var report_tests = []struct {
+	path     string
+	expected string
+}{
+	{"../../examples/julia.spdx.json", `34 total packages
 0 total files
 100% have licenses.
 0% have package digest.
@@ -20,25 +20,55 @@ func TestSpdxE2eReport(t *testing.T) {
 0% have CPEs.
 0% have file digest.
 Spec valid? true
-Has creation info? false` {
-		t.Log("Incorrect report results generated.\n" +
-			"Got this: \n" + report_text)
-		t.Fail()
+Has creation info? false`},
+	{"../../examples/photon.spdx.json", `38 total packages
+0 total files
+94% have licenses.
+2% have package digest.
+97% have package versions.
+0% have purls.
+0% have CPEs.
+0% have file digest.
+Spec valid? true
+Has creation info? true`},
+}
+
+func TestSpdxE2eReport(t *testing.T) {
+	for _, e := range report_tests {
+		res := GetSpdxReport(e.path)
+		report_text := res.Report()
+		if strings.Trim(report_text, " \n") != e.expected {
+			t.Errorf("GetSpdxReport(%v) = %v, expected %v",
+				e.path, strings.Trim(report_text, " \n"), e.expected)
+		}
 	}
 }
 
-func TestSpdxE2eGrade(t *testing.T) {
-	r := GetSpdxReport("../../examples/julia.spdx.json")
-	report_text := scorecard.Grade(r)
-
-	if strings.Trim(report_text, " \n") != `Spec Compliance: 25/25
+var grade_tests = []struct {
+	path     string
+	expected string
+}{
+	{"../../examples/julia.spdx.json", `Spec Compliance: 25/25
 Package ID: 0/20 (0% have purls and 0% have CPEs)
 Package Versions: 0/20
 Package Licenses: 20/20
 Creation Info: 0/15 (No tool was used to create the sbom)
-Total points: 45/100 or 45%` {
-		t.Log("Incorrect report results generated.\n" +
-			"Got this: \n" + report_text)
-		t.Fail()
+Total points: 45/100 or 45%`},
+	{"../../examples/photon.spdx.json", `Spec Compliance: 25/25
+Package ID: 0/20 (0% have purls and 0% have CPEs)
+Package Versions: 19/20
+Package Licenses: 18/20
+Creation Info: 15/15
+Total points: 78/100 or 78%`},
+}
+
+func TestSpdxE2eGrade(t *testing.T) {
+	for _, e := range grade_tests {
+		res := GetSpdxReport(e.path)
+		report_text := scorecard.Grade(res)
+		if strings.Trim(report_text, " \n") != e.expected {
+			t.Errorf("GetSpdxReport(%v) = %v, expected %v",
+				e.path, strings.Trim(report_text, " \n"), e.expected)
+		}
 	}
 }
